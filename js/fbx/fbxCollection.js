ee8eaf6736557ec15463878bbf311006ca5d9b1d
Jellevz <60503294+Jellevz@users.noreply.github.com>
2020-04-28 12:01:45 +0200

    ajout fonctions ALL play pause resume dans fbxCollection et play pause resume dans fbxAnimationElement

--- js/fbx/fbxCollection.js
+++ js/fbx/fbxCollection.js
@@ -3,7 +3,7 @@
  */
 class FBXAnimationArray extends Array {
 
-    /** Retire l'élément de uuid correspondant de la collection
+  /** Retire l'élément de uuid correspondant de la collection
    *  @param {*} uuid_ 
    */
   removeByUUID(uuid_) {
@@ -27,11 +27,141 @@ class FBXAnimationArray extends Array {
     }
   }
 
+  /** Retourne si l'object entré en paramaetre est présent o non dans la liste. 
+   * @param {UUID} objectUuid_ le UUID à rechercher
+   * @returns {Boolean} True si la collection contient un élément correspondant au UUID donné
+   */
+  contains(objectUuid_) {
+    return this.some((fbx_) => {
+      return fbx_.uuid == objectUuid_
+    })
+  }
+
+  /** Mets en pause tous les fbx  */
+  pauseAllAnimations() {
+    this.forEach((fbx) => {
+      fbx.pauseAnimation()
+    })
+  }
+
+  /** Joue tous les fbx */
+  playAllAnimations() {
+    this.forEach((bvh) => {
+      bvh.playAnimation()
+    })
+  }
+
+  /** 
+   *  @returns {Boolean} True si au moins un élément de l'ensemble reprend effectivement son animation, False sinon.
+   */
+  resumeAllAnimations() {
+    let atLeastOneAnimationToPlay = false
+    this.forEach((fbx) => {
+      if (fbx.resumeAnimation() == true) {
+        atLeastOneAnimationToPlay = true
+      }
+    })
+    return atLeastOneAnimationToPlay
+  }
+
+  /** TODO */
+  replayAllAnimations(){
+
+  }
+
 }
 
 /** Objet contenant l'ensembles des données nécéssaires aux traitement d'un BVH.
  *  Utilisé dans BVHAnimationArray
  */
 class FBXAnimationElement {
+  /**
+   * @param {*} name_ le nom du FBX
+   */
+  constructor(name_,fbxFile_){
+    this.isPaused = false
+    this.resumeAnimationValue = this.isPaused
 
+    // Pause/Play
+    this.playPauseButton = $("#" + this.uuid + " .playPause")[0]
+
+    //TODO Time Slider
+  }
+
+
+  /**  */
+  toggleAnimation() {
+    if (this.isPaused) this.playAnimation()
+    else this.pauseAnimation()
+    this.resumeAnimationValue = this.isPaused
+    this._updatePlayPauseImg()
+  }
+
+  /**  */
+  playAnimation() {
+    this.isPaused = false
+    this._updatePlayPauseImg()
+  }
+
+  /**  */
+  pauseAnimation() {
+    this.isPaused = true
+    this._updatePlayPauseImg()
+  }
+
+  /**  */
+  resumeAnimation() {
+    this.isPaused = this.resumeAnimationValue
+    this._updatePlayPauseImg()
+    return !this.isPaused
+  }
+
+  /** TODO */
+  hide() {
+  }
+
+  /** TODO */
+  show() {
+  }
+
+  /** TODO */
+  replayAnimation(){
+
+  }
+
+  //** TODO */
+  modifyTimeSlider(){
+
+  }
+  
+  /** Met a jour l'image du bouton playPause
+   *  
+   *  Met l'image de pause si l'object passe en lecture
+   *  Met l'image de lecture si l'object passe en pause
+   */
+  _updatePlayPauseImg() {
+    let img = $('#' + this.uuid + " .playPause")[0].lastChild
+    if (this.isPaused) {
+      img.src = "./images/play_button.svg"
+    } else {
+      img.src = "./images/pause_button.svg"
+    }
+  }
+
+  /** Retourne si le FBX est en pause ou non */
+  get isPaused() {
+    return this._isPaused
+  }
+
+  /**  */
+  set isPaused(newValue) {
+    if (newValue == false || newValue == true) {
+      this._isPaused = newValue
+    }
+  }
+
+  /** Retourne l'uuid du FBX */
+  get uuid() {
+    return this.skeleton.uuid
+  }
 }
