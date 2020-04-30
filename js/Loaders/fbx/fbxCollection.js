e08604c990ddcfa156c586bad0b30fb8eb03ece8
D4shw00d <valentin5629@gmail.com>
2020-04-30 17:35:57 +0200

    separation Collection en Collection + Elements pour BVHCollection.js et FBXCollection.js

--- js/Loaders/fbx/fbxCollection.js
+++ js/Loaders/fbx/fbxCollection.js
@@ -1,17 +1,2 @@
-/**
- * Array avec des méthodes et attributs en plus dans le but de stocker et d'interagir avec une collection de FBX.
- */
+/** Array avec des méthodes et attributs en plus dans le but de stocker et d'interagir avec une collection de FBX. */
 class FBXAnimationArray extends AnimationArray {}
-
-/** Objet contenant l'ensembles des données nécéssaires aux traitement d'un BVH.
- *  Utilisé dans BVHAnimationArray
- */
-class FBXAnimationElement extends AnimationElement {
-  /**
-   *  @param {*} name_ le nom du FBX
-   */
-  constructor(name_, uuid_, animationMixer_) {
-    super(name_, uuid_, animationMixer_, 0, animationMixer_._root.animations[0].duration)
-    this.frameTime = 1
-  }
-}
